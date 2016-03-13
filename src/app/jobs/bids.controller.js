0ab23d0eeae4a1a5eaf6bc567afe146c8caa303a
Andrew Fletcher <afletcher04@qub.ac.uk>
2016-03-13 07:11:57 +0000

    Adding add bid page.

--- src/app/jobs/bids.controller.js
+++ src/app/jobs/bids.controller.js
@@ -3,126 +3,36 @@
 
   angular
     .module('desktop')
-    .controller('StatusController', StatusController);
+    .controller('BidController', BidController);
 
   /** @ngInject */
-  function StatusController($scope, $rootScope, $location, $stateParams, $state, Job, Bid) {
+  function BidController($scope, $rootScope, $location, $stateParams, $state, Job, Bid) {
 
-  	var userId = localStorage.getItem("userID");
+    var userId = localStorage.getItem("userID");
 
-  	if (userId == null)
-  	{
-  		$location.path("/app/account/login/app.status");
-  	}
+    if (userId == null)
+    {
+      $location.path("/app/account/login/app.status");
+    }
 
-  	var job_id = $stateParams.id;
+    var job_id = $stateParams.id;
 
-  	$scope.job = Job.findById({ id: job_id}, function(job) {
+    $scope.job = Job.findById({ id: job_id});
 
-  		$scope.winner = Job.bids({
-  			id: $scope.job.id,
-  			filter: {
-  			    where: { winning_bid: true },
-  			  }
-  			 });
+    $scope.submitBid = function() {
 
-  		$scope.biddingComplete = job.bidding_complete;
+      var model = $scope.bid; 
 
-	  	 if(userId == job.customer_id){
-	  	 	$scope.showCustomer = true;
-	  	 } else if (userId == job.business_id){
-	  	 	$scope.showBusiness = true;
-	  	 } else if (userId == $scope.winner.user_id){
-	  	 	$scope.showCourier = true;
-	  	 };
+      model.user_id = userId; 
+      model.job_id = job_id;
 
-	  	 if($scope.job.accepted_by_courier == false){
-	  	 	$scope.collected = false;
-	  	 } else {
-	  	 	$scope.collected = true;
-	  	 };
+      Bid.create(model);
 
+      $location.path("/app/jobs/my");
 
-	  	 if($scope.job.dropped_by_courier == false){
-	  	 	$scope.complete = false;
-	  	 }
-	  	 else {
-	  	 	$scope.complete = true;
-	  	 }
-
-
-	  	 if($scope.job.collected_from_business == false){
-	  	 	$scope.picked_up = false;
-	  	 }
-	  	 else {
-	  	 	$scope.picked_up = true;
-	  	 }
-
-
-	  	 if($scope.job.received_by_customer == false){
-			$scope.received = false;
-	  	 }
-	  	 else {
-	  	 	$scope.received = true; 
-	  	 }
-
-		// $scope.bids = Bid.find({
-		//   filter: { id: job.id }
-		// });
-
-		$scope.bids = Job.bids({
-		  id: $scope.job.id,
-		});	  
-
-
-  	}, function(err) {
-  		// $scope.showCourier = true;
-
-  	});
-
-
-  	$scope.doAcceptedFromBusiness = function() {
-  		$scope.collected = true;
-  		$scope.job.accepted_by_courier = true;
-  		$scope.job.$save();
-  	};
-
-  	$scope.doCompletedDelivery = function() {
-  		$scope.complete = true;
-  		$scope.job.dropped_by_courier = true;
-  		$scope.job.$save();
-  	};
-
-  	$scope.doCollectedByCourier = function() {
-  		$scope.picked_up = true;
-  		$scope.job.collected_from_business = true;
-  		$scope.job.$save();
-  	};
-
-  	$scope.doReceivedByCustomer = function() {
-  		$scope.received = true;
-  		$scope.job.received_by_customer = true;
-  		$scope.job.$save();
-  	};
-
-  	$scope.doSelectedByCustomer = function(bid){
-
-  		Job.findById({ id: bid.job_id}, function(res) {
-
-  			bid.winning_bid = true; 
-  			bid.$save();
-
-  			res.bidding_complete = true; 
-  			res.$save();
-
-  			$state.go($state.current, {}, {reload: true});
-
-  		}, function(err) {
-
-  		});
-
-  	}
+    }; 
 
 
   }
+  
 })();
